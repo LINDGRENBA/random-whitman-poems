6c545576d829ced2671424c62e47f83a2e2280d6
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-10-16 11:15:12 -0700

    Add comments to js file for API calls, add setup to README, ubdate html file with boilerplate.

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+
+Setup
+
+To use this project on your local machine, you will need an API key for the Walt Whitman Poems API
+1. Navigate to the [RapidAPI website](https://rapidapi.com) and set up an account.
+2. Search for the [Walt Whitman Poems API](https://rapidapi.com/pafmon/api/walt-whitman-poems/details) and select the the Walt Whitman Poems result.
+3. Click on the `endpoints` link and you will find your API Key.
+
+To securely add your API Key to the project, follow the steps below
+1. Create a .env file. Once added, this file should appear greyed out (ADD IMAGE)
+2. Add the following to the .env file: API_KEY = "YOUR API KEY HERE", replacing the text between the " " with your actual API Key. 
+3. You should now be able to run the program and use this on your local machine.
---
+++ scripts.js
@@ -0,0 +1,40 @@
+
+
+//GET LIST OF POEMS - TITLES
+
+// var unirest = require("unirest");
+
+// var req = unirest("GET", "https://rapidapi.p.rapidapi.com/poems/");
+
+// req.headers({
+// 	"x-rapidapi-host": "pafmon-walt-whitman-poems.p.rapidapi.com",
+// 	"x-rapidapi-key": "dee4ba2a19msh0b3107c23a70fdfp1cc2cdjsnc07681ab3e06",
+// 	"useQueryString": true
+// });
+
+
+// req.end(function (res) {
+// 	if (res.error) throw new Error(res.error);
+
+// 	console.log(res.body);
+// });
+
+
+//GET POEMS FROM TITLES
+// var unirest = require("unirest");
+
+// var req = unirest("GET", "https://rapidapi.p.rapidapi.com/poems/o-me-o-life");
+// replace o-me-o-life with the title of the poem you would like to see
+
+// req.headers({
+// 	"x-rapidapi-host": "pafmon-walt-whitman-poems.p.rapidapi.com",
+// 	"x-rapidapi-key": "dee4ba2a19msh0b3107c23a70fdfp1cc2cdjsnc07681ab3e06",
+// 	"useQueryString": true
+// });
+
+
+// req.end(function (res) {
+// 	if (res.error) throw new Error(res.error);
+
+// 	console.log(res.body);
+// });
